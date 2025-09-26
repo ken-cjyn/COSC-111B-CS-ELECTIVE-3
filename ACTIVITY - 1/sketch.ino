1222a75007edc11b6adde44e9335ad5a94be039b
ken <114287325+ken-cjyn@users.noreply.github.com>
2025-09-26 20:44:55 +0800

    Add files via upload

---
+++ ACTIVITY - 1/sketch.ino
@@ -0,0 +1,24 @@
+
+void setup()
+{
+  for(int pin = 8; pin <= 12; ++pin)
+  {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void loop()
+{
+
+  for(int pin = 12; pin >= 8; --pin){
+    digitalWrite(pin, HIGH);
+    delay(1000);
+  }
+
+  for(int pin = 12; pin >= 8; --pin){
+    digitalWrite(pin, LOW);
+    delay(1000);
+  }
+}
+
+
